6893de55e9f56a7be556fc24f19a82f003c1dc70
arjunbisht471 <142390689+arjunbisht471@users.noreply.github.com>
2024-06-14 17:42:30 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,51 @@
+AOS.init();
+// You can also pass an optional settings object
+// below listed default settings
+AOS.init({
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 700, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
+// swipper
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    loop: true,
+    autoplay: {
+        delay: 3000,
+    },
+});
+// mail send
+emailjs.init('6WHYOZSIeuLfoTwTh')
+const btn = document.getElementById('form_submit_btn');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_jvc232c';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
+
+// pwa webapp
+
+
